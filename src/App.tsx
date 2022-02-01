ec1f780e4a528ec96aa7fa02768ad782c48c6b96
Nikhil Cheerla <nikcheerla@gmail.com>
2022-02-01 00:30:45 -0800

    Started code

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import Button from "@mui/material/Button";
+
+// randomly generate a user ID every time you join the room
+// you don't need persistence between browser reloads or different sessions,
+// so a random ID will do to distinguish between two tabs with the Youtube Watch Party Open
+const USER_ID = uuidv4();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Welcome to the Youtube Watch Party. Your ID for this session is{" "}
+          {USER_ID}.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button> Add a youtube video</Button>
       </header>
     </div>
   );
