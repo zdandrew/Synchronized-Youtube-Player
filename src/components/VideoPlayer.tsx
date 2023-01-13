fcdde7499543ebc2fd9f058bee1c1de4fca2bdb9
Nikhil Cheerla <nikcheerla@gmail.com>
2023-01-13 02:28:51 -0800

    Update video player

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -10,7 +10,7 @@ interface VideoPlayerProps {
 const VideoPlayer: React.FC<VideoPlayerProps> = ({ url, hideControls }) => {
   const [hasJoined, setHasJoined] = useState(false);
   const [isReady, setIsReady] = useState(false);
-  const player = useRef<ReactPlayer>(null!);
+  const player = useRef<ReactPlayer>(null);
 
   const handleReady = () => {
     setIsReady(true);
@@ -18,6 +18,46 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ url, hideControls }) => {
 
   const handleEnd = () => {
     console.log("Video ended");
+  };
+
+  const handleSeek = (seconds: number) => {
+    // Ideally, the seek event would be fired whenever the user moves the built in Youtube video slider to a new timestamp.
+    // However, the youtube API no longer supports seek events (https://github.com/cookpete/react-player/issues/356), so this no longer works
+
+    // You'll need to find a different way to detect seeks (or just write your own seek slider and replace the built in Youtube one.)
+    // Note that when you move the slider, you still get play, pause, buffer, and progress events, can you use those?
+
+    console.log(
+      "This never prints because seek decetion doesn't work: ",
+      seconds
+    );
+  };
+
+  const handlePlay = () => {
+    console.log(
+      "User played video at time: ",
+      player.current?.getCurrentTime()
+    );
+  };
+
+  const handlePause = () => {
+    console.log(
+      "User paused video at time: ",
+      player.current?.getCurrentTime()
+    );
+  };
+
+  const handleBuffer = () => {
+    console.log("Video buffered");
+  };
+
+  const handleProgress = (state: {
+    played: number;
+    playedSeconds: number;
+    loaded: number;
+    loadedSeconds: number;
+  }) => {
+    console.log("Video progress: ", state);
   };
 
   return (
@@ -36,12 +76,17 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ url, hideControls }) => {
         flexDirection="column"
       >
         <ReactPlayer
-          ref={(ref) => (player.current = ref!)}
+          ref={player}
           url={url}
           playing={hasJoined}
           controls={!hideControls}
           onReady={handleReady}
           onEnded={handleEnd}
+          onSeek={handleSeek}
+          onPlay={handlePlay}
+          onPause={handlePause}
+          onBuffer={handleBuffer}
+          onProgress={handleProgress}
           width="100%"
           height="100%"
           style={{ pointerEvents: hideControls ? "none" : "auto" }}
